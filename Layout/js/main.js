4ed84918f4d8cfb82eb1b59612d587c63aff473a
Paul Pessoa <paulmspessoa@gmail.com>
2021-07-08 17:22:13 -0300

    jquery popup function after submit

--- Layout/js/main.js
+++ Layout/js/main.js
@@ -1,3 +1,15 @@
 //show current year
 const d = new Date();
 document.getElementById("year").innerHTML = d.getFullYear();
+
+//show "thanks popup"
+const myForm = document.getElementById("book-form");
+myForm.addEventListener ("submit", (e) => {
+    e.preventDefault();
+    $("#BlockUIConfirm").fadeIn();
+});
+
+//close / hide "thanks popup"
+function Submit() {
+    $("#BlockUIConfirm").fadeOut();
+}
